a2aeaba6a6c48f764dc289f6eea902286ad7f6a8
pvtfurkan <furki304@gmail.com>
2023-12-30 23:52:15 +0300

    v5

--- app/1/route.ts
+++ app/1/route.ts
@@ -6,8 +6,41 @@ export async function GET(request: Request) {
         // Prisma kullanarak veritabanından veri çekme
         const loadData = await prisma.load.findMany();
 
+        console.log(loadData)
+
         // Çekilen verileri döndürme
-        return NextResponse.json({ data: loadData });
+        return NextResponse.json({ data: [
+            {
+              id: '659068b611b265978e6386e5',
+              name: 'Metal Anahtarlık',
+              state: false,
+              description: 'Metal anahtarlık. Lazer baskıya ve plasto etikete uygundur.',
+              price: 23,
+              brand: 'IL',
+              category: 'Diğer Promosyon Ürünler',
+              inStock: true
+            },
+            {
+              id: '659076a6f4def7d4df34ac20',
+              name: '16gb usb',
+              state: true,
+              description: '16gb usb',
+              price: 270,
+              brand: 'PL',
+              category: 'Diğer Promosyon Ürünler',
+              inStock: true
+            },
+            {
+              id: '659076eff4def7d4df34ac21',
+              name: 'Şapka - Erkek',
+              state: true,
+              description: 'Şapka Erkek',
+              price: 110,
+              brand: 'ET',
+              category: 'Diğer Promosyon Ürünler',
+              inStock: true
+            }
+          ] });
     } catch (error) {
         console.error("An error occurred:", error);
         return NextResponse.json({ error: "An error occurred while processing" }, { status: 500 });

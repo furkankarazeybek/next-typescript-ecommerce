bf692fe583cc931aaf31f6e9e1768173ddbdb20b
pvtfurkan <furki304@gmail.com>
2024-02-02 22:20:20 +0300

    v11

--- app/components/contact/GetInTouch.tsx
+++ app/components/contact/GetInTouch.tsx
@@ -41,52 +41,9 @@ const GetInTouch = () => {
     <div className="pt-10 pb-16">
       <div className="grid grid-cols-1 md:grid-cols-2">
         <div className="h-full w-full flex-1 basis-[18rem] pl-20">
-          <img src="/mg_promosyon.png" alt="" className="w-full h-full" />
+          <img src="/resim.jpg" alt="" className="w-full h-full" />
         </div>
-        <div className="flex-1 basis-[18rem] bg-secondary py-6 !text-slate-200">
-          <div className="max-w-[350px] w-full mx-auto bg-[#0a5076] p-3 rounded-lg">
-            <h1 className="text-lg font-semibold">Bize Ulaşın</h1>
-            <p>Tüm promosyon ürünleri ile ilgili bize ulaşabilirsiniz.</p>
-            <div className="mt-4">
-              <input
-                type="text"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                className="w-full px-2 py-1 border-none rounded-md outline-none text-black"
-                placeholder="Ad Soyad"
-              />
-              <input
-                type="text"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="w-full px-2 py-1 mt-3 border-none rounded-md outline-none text-black"
-                placeholder="Email"
-              />
-              <input
-                type="text"
-                name="tel_no"
-                value={formData.tel_no}
-                onChange={handleChange}
-                className="w-full px-2 py-1 mt-3 border-none rounded-md outline-none text-black"
-                placeholder="Telefon"
-              />
-              <textarea
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                className="w-full p-2 mt-3 border-none rounded-md outline-none text-black"
-                rows={3}
-                placeholder="Mesajınız"
-              ></textarea>
-              <button onClick={handleSubmit} className="w-full mt-4 bg-black p-2 rounded-md">
-                Gönder
-              </button>
-              {message && <div>{message}</div>}
-            </div>
-          </div>
-        </div>
+       
       </div>
     </div>
   );
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,44 @@
-import Image from "next/image";
+'use client'
+
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 import { FiMail, FiPhone } from "react-icons/fi";
 import { BiMap } from "react-icons/bi";
 import GetInTouch from "../components/contact/GetInTouch";
 const Contact = () => {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    tel_no: '',
+    message: '',
+  });
+  const [message, setMessage] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://admin.calikgida.com.tr/api/contactsend', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.status === 200) {
+        setMessage('Mesajınız gönderildi.');
+      } else {
+        setMessage('Bir hata oluştu. Lütfen tekrar deneyin.');
+      }
+    } catch (error) {
+      setMessage('Bir hata oluştu. Lütfen tekrar deneyin.');
+    }
+  };
     return (
         <>
             <div className="relative px-10" style={{ backgroundImage: 'url("/arkplan.png")', backgroundSize: 'cover', backgroundPosition: 'center', position: 'relative',boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)' }}>
@@ -19,59 +55,105 @@ const Contact = () => {
         
        <div className=" w-full sm:w-1/2 py-20 px-20">
 
-       <div className="grid grid-cols-1 gap-6 pt-20 sm:grid-cols-2 md:grid-cols-3">
-
-        <div className="text-center ">
-           <div className="icon-box !h-14 !w-14 !bg-primary text-black mx-auto text-4xl">
-             <BiMap />  
+       <div className="flex-1 basis-[18rem] bg-secondary py-6 !text-white">
+          <div className="max-w-[800px] w-full mx-auto bg-[#131a23] p-3 rounded-lg">
+            <h1 className="text-lg font-semibold">Bize Ulaşın</h1>
+            <p>Tüm promosyon ürünleri ile ilgili bize ulaşabilirsiniz.</p>
+            <div className="mt-4">
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className="w-full px-2 py-1 border-none rounded-md outline-none text-black"
+                placeholder="Ad Soyad"
+              />
+              <input
+                type="text"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="w-full px-2 py-1 mt-3 border-none rounded-md outline-none text-black"
+                placeholder="Email"
+              />
+              <input
+                type="text"
+                name="tel_no"
+                value={formData.tel_no}
+                onChange={handleChange}
+                className="w-full px-2 py-1 mt-3 border-none rounded-md outline-none text-black"
+                placeholder="Telefon"
+              />
+              <textarea
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                className="w-full p-2 mt-3 border-none rounded-md outline-none text-black"
+                rows={10}
+                placeholder="Mesajınız"
+              ></textarea>
+              <button onClick={handleSubmit} className="w-full mt-4 bg-[#febb6e] p-2 rounded-md text-black">
+                Gönder
+              </button>
+              {message && <div>{message}</div>}
+            </div>
           </div>
-          <h1 className="mt-2 text-lg font-semibold">Ofis Adresi</h1>
-          <p>Haydar Çavuş Mahallesi</p>
-          <p>Şehit Süleyman Bey Caddesi</p>
-          <p>10200, Bandırma/Balıkesir</p>
+        </div>
 
-         </div>
-        
-         <div className="text-center">
-           <div className="icon-box !h-14 !w-14 !bg-primary text-black mx-auto text-4xl">
-             <FiPhone />  
-          </div>
-          <h1 className="mt-2 text-lg font-semibold">Telefon</h1>
-            <p>0850 309 3925</p>
-            
-         </div>
-
-
-         <div className="text-center">
-           <div className="icon-box !h-14 !w-14 !bg-primary text-black mx-auto text-4xl">
-             <FiMail />  
-          </div>
-          <h1 className="mt-2 text-lg font-semibold">E-Posta</h1>
-            <p>info@mgpromosyon.com</p>
-            
-         </div>
-        
-         </div>
+       
        </div>  
 
-       <div className="w-full  sm:w-1/2  py-20 px-10">
+       <div className="w-full  sm:w-1/2  pt-20 pb-10 px-10">
 
        <iframe src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Haydar%20%C3%87avu%C5%9F%20Mh.%20%C5%9Eehit%20s%C3%BCleymanbey%20Cad.%20No:10/B+(Haydar%20%C3%87avu%C5%9F%20Mh.%20%C5%9Eehit%20s%C3%BCleymanbey%20Cad.%20No:10/B)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
     
-          width="80%"
+          width="100%"
            height="400"
            
           
         
                 
        /> 	
+         <div className="grid grid-cols-1 gap-6 pt-20 sm:grid-cols-2 md:grid-cols-3">
+
+
+         <div className="text-center ">
+            <div className="icon-box !h-14 !w-14 !bg-primary text-black mx-auto text-4xl">
+              <BiMap />  
+           </div>
+           <h1 className="mt-2 text-lg font-semibold">Ofis Adresi</h1>
+           <p>Haydar Çavuş Mahallesi</p>
+           <p>Şehit Süleyman Bey Caddesi</p>
+           <p>10200, Bandırma/Balıkesir</p>
          
+          </div>
+         
+          <div className="text-center">
+            <div className="icon-box !h-14 !w-14 !bg-primary text-black mx-auto text-4xl">
+              <FiPhone />  
+           </div>
+           <h1 className="mt-2 text-lg font-semibold">Telefon</h1>
+             <p>0850 309 3925</p>
+             
+          </div>
+         
+         
+          <div className="text-center">
+            <div className="icon-box !h-14 !w-14 !bg-primary text-black mx-auto text-4xl">
+              <FiMail />  
+           </div>
+           <h1 className="mt-2 text-lg font-semibold">E-Posta</h1>
+             <p>info@mgpromosyon.com</p>
+             
+          </div>
+         
+          </div>
         </div>  
 
 
        </div>
 
-       <GetInTouch />
+   
         
         </>
 
